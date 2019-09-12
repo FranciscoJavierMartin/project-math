741576b0ff9b16d3e97b23cc6233b1d0693d97cc
Francisco <javiermartincuenca@gmail.com>
2019-09-12 14:22:13 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+    /**
+     * Add two number
+     * @example
+     * n1 = 1, n2 = 3 => result = 4
+     * @param {*} n1 First number
+     * @param {*} n2 Second number
+     */
+    add: function (n1,n2){
+        return n1+n2;
+    },
+    /**
+     * Add two number
+     * @example
+     * n1 = 1, n2 = 3 => result = -2
+     * @param {*} n1 First number
+     * @param {*} n2 Second number
+     */
+    substract: function (n1,n2){
+        return n1-n2;
+    },
+    /**
+     * Multiply two number
+     * @example
+     * n1 = 2, n2 = 3 => result = 6
+     * @param {*} n1 First number
+     * @param {*} n2 Second number
+     */
+    multiply: function (n1,n2){
+        return n1*n2;
+    },
+    /**
+     * Divide two number
+     * @example
+     * n1 = 8, n2 = 4 => result = 2
+     * @param {*} n1 First number
+     * @param {*} n2 Second number
+     */
+    divide: function (n1,n2){
+        return n1/n2;
+    },
+}
